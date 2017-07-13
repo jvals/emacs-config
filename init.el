385ef2bb64abdccf4b14024a2af8798d0bfec8ab
jvals <jvals@users.noreply.github.com>
2017-07-13 18:03:27 +0200

    Disable desktop save mode

--- init.el
+++ init.el
@@ -403,14 +403,14 @@
 ;;   :defer t)
 ;; (golden-ratio-mode 1)
 
-(use-package desktop
-  :ensure t
-  :defer t)
-;; Save desktop
-(desktop-save-mode 1)
-(setq desktop-restore-frames t)
-(setq desktop-restore-in-current-display t)
-(setq desktop-restore-forces-onscreen nil)
+;; (use-package desktop
+;;   :ensure t
+;;   :defer t)
+;; ;; Save desktop
+;; (desktop-save-mode 1)
+;; (setq desktop-restore-frames t)
+;; (setq desktop-restore-in-current-display t)
+;; (setq desktop-restore-forces-onscreen nil)
 
 ;; Activate markdown-preview-mode
 ;; (add-hook 'markdown-mode-hook 'markdown-preview-mode)
