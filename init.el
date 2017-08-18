f13f18e25a8a5ab29ab5bba9720c3c39881b28d0
jvals <jvals@users.noreply.github.com>
2017-08-18 07:44:07 +0200

    Office font unavailable after tanking

--- init.el
+++ init.el
@@ -503,7 +503,8 @@
     (set-face-attribute 'default nil :family "source code pro" :height 110)))
  ((string-equal system-type "gnu/linux") ; linux
   (progn
-    (set-face-attribute 'default nil :family "Office Code Pro" :height 75))))
+    ;;(set-face-attribute 'default nil :family "Office Code Pro" :height 75))))
+    (set-face-attribute 'default nil :family "Dejavu Sans Mono" :height 75))))
 
 (cond
  ((string-equal system-type "gnu/linux")
