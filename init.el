9a5a7ef2e55c0af8ac67807a324006feda189029
jvals <jvals@users.noreply.github.com>
2017-08-18 07:41:47 +0200

    Stop connecting to melpa every restart

--- init.el
+++ init.el
@@ -40,7 +40,7 @@
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
-(package-refresh-contents)
+;; (package-refresh-contents)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
