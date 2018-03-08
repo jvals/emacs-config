bd64e0935a69639c6b50ed0b4c571c4c64acb11a
jvals <jvals@users.noreply.github.com>
2018-03-08 11:22:42 +0100

    Add modified split function

--- init.el
+++ init.el
@@ -316,6 +316,32 @@
   :ensure t)
 
 ;; ------------------------------------------------------------
+;; my split function (modified split-window-sensibly)
+;; ------------------------------------------------------------
+(defun my-split-window-sensibly (&optional window)
+  (let ((window (or window (selected-window))))
+    (or (and (window-splittable-p window t)
+             ;; Split window horizontally.
+             (with-selected-window window
+               (split-window-right)))
+        (and (window-splittable-p window)
+             ;; Split window vertically.
+             (with-selected-window window
+               (split-window-below)))
+        (and (eq window (frame-root-window (window-frame window)))
+             (not (window-minibuffer-p window))
+             ;; If WINDOW is the only window on its frame and is not the
+             ;; minibuffer window, try to split it vertically disregarding
+             ;; the value of `split-height-threshold'.
+             (let ((split-height-threshold 0))
+               (when (window-splittable-p window)
+                 (with-selected-window window
+                   (split-window-below))))))))
+
+(setq split-window-preferred-function #'my-split-window-sensibly)
+
+
+;; ------------------------------------------------------------
 ;; Misc
 ;; ------------------------------------------------------------
 
